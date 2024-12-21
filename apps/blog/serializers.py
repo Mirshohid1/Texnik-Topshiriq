c510adedaa0f069cec49f7d16c8e6695f1a7eb21
l0n3r <mirshohid1214@gmail.com>
2024-12-21 19:09:17 +0500

    feat: add BlogPostInputSerializer serializer

--- apps/blog/serializers.py
+++ apps/blog/serializers.py
@@ -10,3 +10,9 @@ class BlogPostSerializer(serializers.ModelSerializer):
             'title', 'content', 'author',
             'created_at', 'updated_at', 'is_published',
         )
+
+
+class BlogPostInputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BlogPost
+        fields = ('title', 'content', 'is_published')
