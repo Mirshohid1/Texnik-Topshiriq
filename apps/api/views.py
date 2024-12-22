47ef03bd740316c42ba5d95e6659a7529021b25b
l0n3r <mirshohid1214@gmail.com>
2024-12-23 03:03:16 +0500

    feat: add ViewSet for Comments

--- apps/api/views.py
+++ apps/api/views.py
@@ -29,3 +29,27 @@ class BlogPostViewSet(ModelViewSet):
         if post.author != self.request.user:
             raise PermissionDenied("You can only delete your posts.")
         instance.delete()
+
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return CommentInputSerializer
+        return CommentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        comment = self.get_object()
+        if comment.author != self.request.user:
+            raise PermissionDenied("You can only edit your comments or comments your posts.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        comment = self.get_object()
+        if comment.author != self.request.user:
+            raise PermissionDenied("You can only delete your comments or comments your posts.")
+        instance.delete()
