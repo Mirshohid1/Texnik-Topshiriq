741f3756e99ae17ec8ba847cf41c0edf63e7c3cd
l0n3r <mirshohid1214@gmail.com>
2024-12-23 03:02:48 +0500

    feat: add ViewSet for BlogPosts

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.viewsets import ModelViewSet
+from ..blog.models import BlogPost, Comment
+from ..blog.serializers import (
+    BlogPostSerializer, BlogPostInputSerializer,
+    CommentSerializer, CommentInputSerializer,
+)
 
-# Create your views here.
+
+class BlogPostViewSet(ModelViewSet):
+    queryset = BlogPost.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return BlogPostInputSerializer
+        return BlogPostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        post = self.get_object()
+        if self.request.user != post.author:
+            raise PermissionDenied("You can only edit your posts.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        post = self.get_object()
+        if post.author != self.request.user:
+            raise PermissionDenied("You can only delete your posts.")
+        instance.delete()
