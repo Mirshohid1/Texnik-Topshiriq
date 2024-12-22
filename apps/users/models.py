56ece3ff8bc7cc553bfb347b13300804b24950b6
l0n3r <mirshohid1214@gmail.com>
2024-12-23 03:35:51 +0500

    feat: add CustomUserManager model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', 'admin')
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('role', 'user')
+        return self._create_user(username, email, password, **extra_fields)
+
+    def _create_user(self, username, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractUser):
