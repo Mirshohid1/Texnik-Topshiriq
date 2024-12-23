b2a84470eac6bfdbb4ac917ea446118328d10a1e
l0n3r <mirshohid1214@gmail.com>
2024-12-23 09:23:49 +0500

    fix: add all hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_^-kf#3@$l!e*+g2nns2q6fp5za_wplkp00gyql+aq+s=r2^4o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
