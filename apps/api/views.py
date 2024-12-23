06cf1e3469aa632088bd744be554f0490e04bd58
l0n3r <mirshohid1214@gmail.com>
2024-12-23 05:21:17 +0500

    perf: optimize performance of all views & fix: update logic in CommentViewSet, BlogPostViewset

--- apps/api/views.py
+++ apps/api/views.py
@@ -7,49 +7,49 @@ from ..blog.serializers import (
 )
 
 
-class BlogPostViewSet(ModelViewSet):
+class BaseViewSet(ModelViewSet):
+    serializer_class = None
+    input_serializer_class = None
+    is_comment: bool = False
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return self.input_serializer_class
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        comment = self.get_object()
+        if not self._my_permission(comment):
+            raise PermissionDenied("You don't have permission to edit this object.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if not self._my_permission(instance):
+            raise PermissionDenied("You don't have permission to delete this object.")
+        super().perform_destroy(instance)
+
+    def _my_permission(self, obj):
+        if self.is_comment:
+            return (
+                self.request.user == obj.author or
+                self.request.user.role == 'admin' or
+                self.request.user == obj.post.author
+            )
+        else:
+            return self.request.user == obj.author or self.request.user.role == 'admin'
+
+
+class BlogPostViewSet(BaseViewSet):
     queryset = BlogPost.objects.all()
-
-    def get_serializer_class(self):
-        if self.action in ['create', 'update', 'partial_update']:
-            return BlogPostInputSerializer
-        return BlogPostSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-    def perform_update(self, serializer):
-        post = self.get_object()
-        if self.request.user != post.author:
-            raise PermissionDenied("You can only edit your posts.")
-        serializer.save()
-
-    def perform_destroy(self, instance):
-        post = self.get_object()
-        if post.author != self.request.user:
-            raise PermissionDenied("You can only delete your posts.")
-        instance.delete()
+    serializer_class = BlogPostSerializer
+    input_serializer_class = BlogPostInputSerializer
 
 
-class CommentViewSet(ModelViewSet):
+class CommentViewSet(BaseViewSet):
     queryset = Comment.objects.all()
-
-    def get_serializer_class(self):
-        if self.action in ['create', 'update', 'partial_update']:
-            return CommentInputSerializer
-        return CommentSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-    def perform_update(self, serializer):
-        comment = self.get_object()
-        if comment.author != self.request.user:
-            raise PermissionDenied("You can only edit your comments or comments your posts.")
-        serializer.save()
-
-    def perform_destroy(self, instance):
-        comment = self.get_object()
-        if comment.author != self.request.user:
-            raise PermissionDenied("You can only delete your comments or comments your posts.")
-        instance.delete()
+    serializer_class = CommentSerializer
+    input_serializer_class = CommentInputSerializer
+    is_comment = True
